bf8f59db342e044148f5be4b09bd073f33fa597b
Matthew Whitehorn <whitehornmatthew@gmail.com>
2026-04-22 19:59:12 -0400

    Migrate register tests

--- tests/register/registerUser.spec.ts
+++ tests/register/registerUser.spec.ts
@@ -4,16 +4,11 @@ import testData from '../../data/testData.json';
 const registerUserName = 'TestUser';
 const registerUserEmail = `testuser_${Date.now()}@gmail.com`;
 
-
 test.beforeEach(async ({ homePage }) => {
-    await homePage.visit();
+    await homePage.navigateToLoginPage();
 });
 
-
-test('Register User and Delete user', async ({ homePage, loginPage, registerPage, page }) => {
-  
-  await homePage.homeLoginButton.click();
-  await page.waitForURL(/\/login$/);
+test('Test Case 1: Register User', async ({ loginPage, registerPage}) => {
   await loginPage.registerUser(registerUserName, registerUserEmail);
   await registerPage.fillRegistrationForm(testData.registerUser);
   await registerPage.submitForm();
@@ -23,4 +18,9 @@ test('Register User and Delete user', async ({ homePage, loginPage, registerPage
   await registerPage.deleteAccountButton.click();
   await expect(registerPage.deleteAccountConfirmationMessage).toBeVisible();
   await registerPage.continueButton.click();
+});
+
+test('Test Case 5: Register User with existing email', async ({ loginPage }) => {
+  await loginPage.registerUser(registerUserName, testData.loginUser.email);
+  await expect(loginPage.existingEmailErrorMessage).toBeVisible();
 });
